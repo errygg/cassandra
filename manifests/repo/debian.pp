491067b0abd8bf5715901e49a37eb12648757169
Erik R. Rygg <erik.rygg@stratagemgroup.com>
2015-01-07 09:40:37 -0700

    Changes to support custom deployment

--- manifests/repo/debian.pp
+++ manifests/repo/debian.pp
@@ -10,7 +10,7 @@ class cassandra::repo::debian(
         location          => $location,
         release           => $release,
         repos             => $repos,
-        key               => $repo_name,
+        key               => "B999A372",
         key_source        => $key_source,
         pin               => $pin,
         include_src       => false,
